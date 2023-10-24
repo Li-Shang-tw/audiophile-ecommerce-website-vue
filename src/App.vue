9c700be7c8b90bb6dae271dbdd769ddfb23552b4
shang <vuvu0130@gmail.com>
2023-10-24 15:38:31 +0800

    init navigator component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import TheNavigator from './components/TheNavigator.vue';
 </script>
 
 <template>
- 
+ <TheNavigator/>
   <RouterView />
 </template>
 
