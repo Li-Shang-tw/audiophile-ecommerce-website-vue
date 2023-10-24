430c075f448e70705b8af212661adbe9564c47d8
shang <vuvu0130@gmail.com>
2023-10-24 20:42:08 +0800

    design system setting

--- src/components/TheNavigator.vue
+++ src/components/TheNavigator.vue
@@ -1,5 +1,7 @@
 <template>
-  
+  <h1>h1</h1>
+  <h2>h2</h2>
+  <p class="overline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic modi eum assumenda perferendis quisquam sunt soluta laborum cumque quae rerum necessitatibus officiis nostrum aspernatur, debitis saepe placeat. Laboriosam, mollitia quae?</p>
 </template>
 
 <script setup>
